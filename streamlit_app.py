bf70b8e1fff212f20ee728f25cdbac3bf1ffd57b
Govadi Rahul <govadirahul@gmail.com>
2026-01-12 21:31:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,4 +66,5 @@ st.subheader("🍉 SmoothieFruit Nutrition Info")
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
