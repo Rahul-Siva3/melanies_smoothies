6cde369d7bca6828057538d81c6f59c42e4bb528
Govadi Rahul <govadirahul@gmail.com>
2026-01-12 21:10:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,11 @@ if submit_order and ingredients_list and name_on_order:
         f"Your Smoothie is ordered, {name_on_order}!",
         icon="✅"
     )
+
+# New section to display smoothiefruit nutrition information
+smoothiefruit_response = requests.get(
+    "https://my.smoothiefruit.com/api/fruit/watermelon"
+)
+
+st.text(smoothiefruit_response)
+
